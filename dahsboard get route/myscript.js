38fbfc218d146fb6632465cca35e22b979e2f2aa
cruiz328 <lemmy125@gmail.com>
2022-04-07 09:24:07 -0700

    get routes initial code

---
+++ dahsboard get route/myscript.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(8000, function () {
+  console.log('Listening to Port 8000');
+});
