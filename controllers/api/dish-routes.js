e6e6886795af4148a01a098fc17579c5bb61351b
Dakota Dunn <dakotadunn5@gmail.com>
2022-04-06 17:21:42 -0700

    added getting json from api for recipes by primary key

--- controllers/api/dish-routes.js
+++ controllers/api/dish-routes.js
@@ -2,35 +2,27 @@ const router = require('express').Router();
 const res = require('express/lib/response');
 const { Dish, User } = require('../../models');
 
-router.get('/:num', (req, res) => {
-    const dbData = {
-        dishName: req.body.dishName,
-        recipe: req.body.recipe,
-        ingredients: req.body.ingredients,
-        country: req.body.ingredients
+router.get('/dishes/:id', (req, res) => {
+    try {
+        const dbRecipeData = await Dish.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Dish,
+                    attributes: [
+                        'id',
+                        'dishName',
+                        'recipe',
+                        'ingredients',
+                        'country'
+                    ]
+                }
+            ]
+        })
+        const recipes = dbRecipeData.get({plain: true});
+        res.json('recipes', {recipes, loggedin: req.session.loggedin})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
     }
+    
 })
-
-router.post('/', (req, res) => {
-    const dbUserData = User.create({
-        googleId: req.body.googleId,
-        displayName: req.body.displayName,
-    })
-    req.session.save(() => {
-        req.session.loggedIn = true;
-
-        res.status(200).json(dbUserData);
-    })
-})
-
-router.post('/login', (req, res) => {
-    where:{
-        googleId: req.body.googleId
-    }
-})
-
-if(!dbUserData) {
-    res.status(400)
-    .json({message: 'Not a valid user. Please try again.'})
-    return;
-}
