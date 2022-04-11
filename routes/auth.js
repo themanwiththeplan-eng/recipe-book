4203518c52e5b5533dc353378aa9f284418c03ba
Dakota Dunn <dakotadunn5@gmail.com>
2022-04-10 20:13:40 -0700

    updated home-routes.js, auth.js, and made homepage.hbs

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,8 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
-
-// GET /auth/google  auth with google
-router.get('/google', passport.authenticate('google', { scope: ['profile']}))
-
-//GET /auth/google/callback  Google auth callback
-router.get('/google/callback',   passport.authenticate('google', { failureRedirect: '/',}), (req, res) => {
-    res.redirect('/dashboard')
-})  
-
-//route  /auth/logout
-router.get('/logout', (req,res) => {
-    req.logout()
-    res.redirect('/')
-})
-
-module.exports = router
+const withAuth = (req, res, next) => {
+    if(!req.session.loggedIn) {
+        res.redirect('/login');
+    }else{
+        next();
+    }
+}
+module.exports = withAuth;
