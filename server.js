ee2ea20f3c93a3da0b0887ce4d3b9e4120634afc
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-10 20:19:58 -0700

    update codes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const path = require('path')
 const express = require('express')
+const path = require('path')
+const Router = require('./routes/index')
 const mongoose = require('mongoose')
-const connectDB = require('./config/db')
 const MongoStore = require('connect-mongo')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -10,16 +10,12 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 
-
-
-
 //load config
-dotenv.config({ path: './.env'})
+dotenv.config({ path: './.env' })
 
-//passport config 
+//passport config
 require('./config/passport')(passport)
 
-connectDB()
 
 //Initialze app
 const app = express()
@@ -29,38 +25,37 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //Sessions and store account in MongoDB avoid kicked out
-app.use(session({ 
+app.use(
+  session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({mongoUrl: process.env.MONGO_URI,})
-  }))
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+)
 
 //express handlebars
-app.engine('.hbs', exphbs.engine ({ defaultLayout: 'main', extname: '.hbs',}))
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-//passport middleware 
+//routes
+app.use('/', Router)
+
+//passport middleware
 app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(passport.session());
-
-
-//logging 
+//logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+  app.use(morgan('dev'))
 }
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-//routes
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth')) 
-
-
-
 const PORT = process.env.PORT || 3001
- 
-app.listen(PORT, 
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`))
+
+app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`)
+)
