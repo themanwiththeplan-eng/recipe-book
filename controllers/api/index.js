f940c6b831a6ad987f0bb6373b103107205f9cac
Dakota Dunn <dakotadunn5@gmail.com>
2022-04-10 19:58:07 -0700

    updated schema, updata User.js for login, updated user-routes, updated index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
+const userRoutes = ('./user-routes')
 const dishRoutes = require('./dish-routes');
 
 router.use('/dishes', dishRoutes)
+router.use('/users', userRoutes)
 
 module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').router
+const { render } = require('express/lib/response')
+const { User } = require('../../models')
+
+render.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try{
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+        if(!dbUserData){
+            res.status(400).json({message: 'Incorrect username or password. Please try again!'})
+            return;
+        }
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({message: 'Incorrect username or password. Please try again!'})
+            return;
+        }
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({user: dbUserData, message: 'You are now logged in!'})
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn){
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+})
+
+module.exports = router;
